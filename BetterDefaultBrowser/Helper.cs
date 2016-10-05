cb7ee090728be417b7c67539679bb61b7ac0c2f0
SSauermann <saschasauermann@gmx.de>
2016-10-05 21:46:19 +0200

    V0.0.1b

---
+++ BetterDefaultBrowser/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterDefaultBrowser
+{
+    class Helper
+    {
+        private static void start(String path, String arg) {
+            Process proc = new Process();
+            proc.StartInfo.FileName = path;
+            proc.StartInfo.Arguments = arg;
+            proc.Start();
+            proc.WaitForExit();
+            //System.Threading.Thread.Sleep(1000);
+        }
+
+        public static void startHelper(String mode)
+        {
+            start("BetterDefaultBrowser-Helper.exe", "-" + mode);
+        }
+    }
+}
---
+++ BetterDefaultBrowser/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BetterDefaultBrowserLib;
+using System.Runtime.InteropServices;
+
+namespace BetterDefaultBrowser
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DefaultBrowser dB = new DefaultBrowser();
+        Settings settings = new Settings();
+        public MainWindow()
+        {
+            InitializeComponent();
+            refresh();
+        }
+
+        public void refresh()
+        {
+            dB = new DefaultBrowser();
+            labelDefaultBrowser.Content = dB.GetDefault().Name;
+            listBoxInstalledBrowsers.Items.Clear();
+            foreach (var browser in dB.Browsers)
+            {
+                listBoxInstalledBrowsers.Items.Add(browser);
+            }
+
+
+            if (dB.isInstalled())
+            {
+                btnInstallBrowser.IsEnabled = false;
+                btnUninstallBrowser.IsEnabled = true;
+            }
+            else
+            {
+                btnInstallBrowser.IsEnabled = true;
+                btnUninstallBrowser.IsEnabled = false;
+            }
+        }
+
+        private void listBoxInstalledBrowsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBoxInstalledBrowsers.SelectedItem != null) {
+                btnSetDefault.IsEnabled = true;
+                btnTestOpen.IsEnabled = true;
+                btnSetProxyDefault.IsEnabled = true;
+                btnSetProxyFilter.IsEnabled = true;
+            }
+            else
+            {
+                btnSetDefault.IsEnabled = false;
+                btnTestOpen.IsEnabled = false;
+                btnSetProxyDefault.IsEnabled = false;
+                btnSetProxyFilter.IsEnabled = false;
+            }
+
+        }
+
+        private void btnTestOpen_Click(object sender, RoutedEventArgs e)
+        {
+            (listBoxInstalledBrowsers.SelectedItem as Browser).StartWithWebsite(@"http://www.google.de");
+        }
+
+        private void btnSetDefault_Click(object sender, RoutedEventArgs e)
+        {
+            dB.SetDefault((listBoxInstalledBrowsers.SelectedItem as Browser));
+
+            refresh();
+        }
+
+        private void btnTestOpen_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.google.de");
+        }
+
+        private void btnInstallBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            Helper.startHelper("install " + AppDomain.CurrentDomain.BaseDirectory +  "BetterDefaultBrowser-Proxy.exe");
+            btnInstallBrowser.IsEnabled = false;
+            btnUninstallBrowser.IsEnabled = true;
+            refresh();
+        }
+
+        private void btnUninstallBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            Helper.startHelper("uninstall");
+            btnInstallBrowser.IsEnabled = true;
+            btnUninstallBrowser.IsEnabled = false;
+            refresh();
+        }
+
+        private void btnSetProxyFilter_Click(object sender, RoutedEventArgs e)
+        {
+            var filters = settings.Filter;
+            filters.AddLast(new Filter(textBox.Text, listBoxInstalledBrowsers.SelectedItem as Browser));
+            settings.Filter = filters;
+        }
+
+        private void btnSetProxyDefault_Click(object sender, RoutedEventArgs e)
+        {
+            settings.DefaultBrowser = listBoxInstalledBrowsers.SelectedItem as Browser;
+        }
+
+        private void btnDeleteSettings_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\BetterDefaultBrowser",true);
+            Application.Current.Shutdown();
+        }
+    }
+}
