6f1156647223cf124d72f5dd07471d74f83a8e1f
S. Sauermann <saschasauermann@gmx.de>
2016-10-06 02:23:38 +0200

    Added logging and class for hardcoded values.

--- BetterDefaultBrowser-Proxy/Program.cs
+++ BetterDefaultBrowser-Proxy/Program.cs
@@ -2,63 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using BetterDefaultBrowserLib;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+using BetterDefaultBrowserLib.Debug;
 
-namespace Proxy
+namespace BetterDefaultBrowser.Proxy
 {
+
+    /// <summary>
+    /// Usage: Proxy.exe &lt;URL&gt;
+    /// </summary>
     static class Program
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool AllocConsole();
-
         [STAThread]
         static void Main()
         {
-
-            //AllocConsole();
-            //Console.WriteLine("Test");
-            string[] args = Environment.GetCommandLineArgs();
-
-            var url = "";
-            if (args.Length > 1)
-                url = args[1];
-
-
-            //Console.WriteLine(url);
-
-            Settings settings = new Settings();
-
-            var filters = settings.Filter;
-            //Console.WriteLine(filters);
-            var defBrowser = settings.DefaultBrowser;
-
-            var selBrowser = defBrowser;
-
-            foreach(var filter in filters)
+            try
             {
-                if (filter.match(url))
+                DebugHelper.SetUpListener();
+
+                //Process command line arguments
+                string[] args = Environment.GetCommandLineArgs();
+
+                //Use first argument as url or empty string if missing
+                var url = "";
+                if (args.Length > 1)
+                    url = args[1];
+
+                Debug.WriteLine("Information: " + "Url to open: '" + url + "'");
+
+                //Read settings for filter and the default browser
+                Settings settings = new Settings();
+
+                var filters = settings.Filter;
+
+                Debug.Assert(filters != null, "Filter list is null");
+
+                var defBrowser = settings.DefaultBrowser;
+
+                //Debug.Assert(defBrowser != null, "Default browser is null");
+                if (defBrowser == null)
                 {
-                    selBrowser = filter.AssignedBrowser;
-                    break;
+                    Trace.TraceWarning("Default browser is not set!");
+                    Environment.Exit(1);
                 }
-            }
 
-            //Loop catching
-            if (selBrowser.Name == "BDB Proxy")
+                //Select a browser based on the filters
+                var selBrowser = defBrowser;
+
+                foreach (var filter in filters)
+                {
+                    if (filter.match(url))
+                    {
+                        selBrowser = filter.AssignedBrowser;
+                        break;
+                    }
+                }
+
+                Debug.WriteLine("Information: " + "Matching browser: '" + selBrowser.Name + "'");
+
+                //Loop catching
+                if (selBrowser.Name == HardcodedValues.BROWSER_NAME)
+                {
+                    throw new ApplicationException("No wormholes allowed.");
+                }
+
+                //Start browser
+                Process proc = new Process();
+                proc.StartInfo.FileName = selBrowser.ApplicationPath;
+                proc.StartInfo.Arguments = url;
+                proc.Start();
+
+                Debug.WriteLine("Information: " + "Browser opened.");
+            }
+            catch (Exception ex)
             {
-                throw new ApplicationException("No wormholes allowed.");
+                Trace.TraceError(ex.ToString());
+                Environment.Exit(2);
             }
-
-            Process proc = new Process();
-            proc.StartInfo.FileName = selBrowser.ApplicationPath;
-            proc.StartInfo.Arguments = url;
-            proc.Start();
-
-            //Console.ReadKey();
+            finally
+            {
+                Trace.Close();
+            }
         }
     }
 }
---
+++ BetterDefaultBrowserLib/HardcodedValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterDefaultBrowserLib
+{
+    public static class HardcodedValues
+    {
+        /// <summary>
+        /// App name for usage in paths.
+        /// </summary>
+        public static String APP_NAME_PATH = "BetterDefaultBrowser";
+
+        /// <summary>
+        /// App name for usage in display.
+        /// </summary>
+        public static String APP_NAME  = "Better Default Browser";
+
+        /// <summary>
+        /// Path to the folder in which settings and log files are stored. Ending with \.
+        /// </summary>
+        public static String DATA_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\" + APP_NAME_PATH + @"\";
+
+        /// <summary>
+        /// Browser name as it will be set in the registry.
+        /// </summary>
+        public static String BROWSER_NAME = "BDB Proxy";
+    }
+}
