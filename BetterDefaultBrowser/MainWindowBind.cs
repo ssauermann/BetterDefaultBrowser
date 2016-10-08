a08b433852186745ee709bda9f63beccd957a18f
Alexander Frank <alexander-pascal.frank@tum.de>
2016-10-08 22:37:29 +0200

    Added new GUI structure for managed filter

--- BetterDefaultBrowser/MainWindowBind.cs
+++ BetterDefaultBrowser/MainWindowBind.cs
@@ -9,69 +9,86 @@ using System.Threading.Tasks;
 
 namespace BetterDefaultBrowser
 {
-    public class MainWindowBind:INotifyPropertyChanged
+    public class MainWindowBind : INotifyPropertyChanged
     {
         private Browser browser;
-        public Browser Browser {
+        public Browser Browser
+        {
             get { return browser; }
-            set { browser = value;
+            set
+            {
+                browser = value;
                 OnPropertyChanged(new PropertyChangedEventArgs("Browser"));
             }
         }
 
-        public enum Protocols { https, http, all};
+        public enum Protocols { https, http, any };
 
         private Protocols protocol;
 
-        public Protocols Protocol {
+        public Protocols Protocol
+        {
             get { return protocol; }
-            set { protocol = value;
+            set
+            {
+                protocol = value;
                 OnPropertyChanged(new PropertyChangedEventArgs("Protocol"));
             }
         }
 
         private string url;
-        public string Url {
+        public string Url
+        {
             get { return url; }
-            set { url = value;
+            set
+            {
+                url = value;
                 OnPropertyChanged(new PropertyChangedEventArgs("Url"));
             }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) {
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
             if (PropertyChanged != null)
                 PropertyChanged(this, e);
         }
 
-        public MainWindowBind(Browser browser,Protocols protocol)
+        public MainWindowBind(Browser browser, Protocols protocol)
         {
             Browser = browser;
-            Protocol= protocol;
+            Protocol = protocol;
         }
 
         public MainWindowBind()
         {
-            Protocol = Protocols.all;
+            Protocol = Protocols.any;
         }
 
         public void saveCurrent()
-        {           
-            StringBuilder str = new StringBuilder();
-            str.Append(ProtocolRegex());
-            str.Append(@"(w{3}\.)?");
-            str.Append(Regex.Escape(url));
-            Filter filter = new Filter(str.ToString(), browser);
-            LinkedList<Filter> ls = Settings.Filter;
-            ls.AddFirst(filter);
-            Settings.Filter = ls;
+        {
+            if (browser != null)
+            {
+                StringBuilder str = new StringBuilder();
+                str.Append(ProtocolRegex());
+                str.Append(@"(w{3}\.)?");
+                str.Append(Regex.Escape(url));
+                Filter filter = new Filter(str.ToString(), browser);
+                LinkedList<Filter> ls = Settings.Filter;
+                ls.AddFirst(filter);
+                Settings.Filter = ls;
+            }
+            else
+            {
+                throw new Exception("user did not mark a browser");
+            }
         }
 
         private string ProtocolRegex()
         {
             switch (protocol)
             {
-                case Protocols.all:
+                case Protocols.any:
                     return @"((h|H)(t|T){2}(p|P)(s|S)?\://)?";
                 case Protocols.http:
                     return @"(h|H)(t|T){2}(p|P)\://";
