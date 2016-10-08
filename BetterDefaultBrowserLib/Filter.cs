f9bc6dafc4bfe646eaa953c3a5a299f901650b40
S. Sauermann <saschasauermann@gmx.de>
2016-10-08 21:31:58 +0200

    Added additional filter types and more attributes for the basic filter.

--- BetterDefaultBrowserLib/Filter.cs
+++ BetterDefaultBrowserLib/Filter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -10,32 +11,34 @@ namespace BetterDefaultBrowser.Lib
     /// <summary>
     /// Filter for request handling. If the filter is matched, the assigned browser will be used to open the url.
     /// </summary>
-    public class Filter
+    public class Filter : INotifyPropertyChanged
     {
-        private Regex regex;
+        #region Attributes & Constructors
+        //Default value: Regex matching nothing
+        private Regex regex = new Regex("(?!x)x");
+        private Browser assignedBrowser;
+        private FType ftype;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        /// Create a new filter with a regular expression and an assigned browser.
+        /// Create a new filter with an assigned browser.
         /// </summary>
-        /// <param name="regex">Regular Expression</param>
         /// <param name="browser">Assigned browser</param>
-        public Filter(String regex, Browser browser)
+        public Filter(Browser browser)
         {
-            this.RegEx = regex;
             this.AssignedBrowser = browser;
         }
-        
-        /// <summary>
-        /// Create a new filter with a regular expression and an assigned browser.
-        /// </summary>
-        /// <param name="regex">Regular Expression</param>
-        /// <param name="browser">Key name of the assigned browser</param>
-        public Filter(String regex, String browser)
-        {
-            this.RegEx = regex;
-            this.AssignedBrowser = new Browser(browser);
-        }
 
+        /// <summary>
+        /// Create a new filter with an assigned browser.
+        /// </summary>
+        /// <param name="browser">Key name of the assigned browser</param>
+        public Filter(String browser) : this(new Browser(browser)) { }
+
+        #endregion
+
+        #region Properties
         /// <summary>
         /// Set a regular expression for filtering requests.
         /// </summary>
@@ -43,16 +46,21 @@ namespace BetterDefaultBrowser.Lib
         {
             get
             {
-               return regex.ToString();
+                return regex.ToString();
             }
             set
             {
                 if (IsValidRegex(value))
                 {
-                    regex = new Regex(value);
-                }else
+                    if (!regex.Equals(value))
+                    {
+                        regex = new Regex(value);
+                        OnPropertyChanged("RegEx");
+                    }
+                }
+                else
                 {
-                    throw new FilterInvalidException("Not valid regex!");
+                    throw new FilterInvalidException("Not a valid regex!");
                 }
             }
         }
@@ -60,8 +68,49 @@ namespace BetterDefaultBrowser.Lib
         /// <summary>
         /// Assigned browser for this filter.
         /// </summary>
-        public Browser AssignedBrowser { get; set; }
+        public Browser AssignedBrowser
+        {
+            get
+            {
+                return this.assignedBrowser;
+            }
+            set
+            {
+                if (!assignedBrowser.Equals(value))
+                {
+                    this.assignedBrowser = value;
+                    OnPropertyChanged("AssignedBrowser");
+                }
+            }
+        }
 
+        /// <summary>
+        /// Filter type
+        /// </summary>
+        public FType Type
+        {
+            get
+            {
+                return this.ftype;
+            }
+            set
+            {
+                if (!ftype.Equals(value))
+                {
+                    this.ftype = value;
+                    OnPropertyChanged("Type");
+                }
+            }
+        }
+
+        /// <summary>
+        /// ID for reference in saved file
+        /// </summary>
+        internal int ID { get; set; }
+
+        #endregion
+
+        #region Methods
         /// <summary>
         /// Match this filter to an url.
         /// </summary>
@@ -72,12 +121,23 @@ namespace BetterDefaultBrowser.Lib
             return regex.IsMatch(url);
         }
 
+        /// <summary>
+        /// Stores the filter into the save file.
+        /// Has to be called after an filters was edited or added.
+        /// </summary>
+        public void store()
+        {
+            Settings.saveFilter(this);
+        }
 
         public override string ToString()
         {
             return "Filter: " + AssignedBrowser + " <-> " + RegEx;
         }
 
+        #endregion
+
+        #region Inner Classes
         /// <summary>
         /// Filter has invalid settings.
         /// </summary>
@@ -95,7 +155,27 @@ namespace BetterDefaultBrowser.Lib
             }
         }
 
+        /// <summary>
+        /// Filter types
+        /// </summary>
+        public enum FType
+        {
+            /// <summary>
+            /// Just plain regex
+            /// </summary>
+            PLAIN,
+            /// <summary>
+            /// Uses first open browser
+            /// </summary>
+            OPEN,
+            /// <summary>
+            /// Fancy GUI for regex creation
+            /// </summary>
+            MANAGED
+        }
+        #endregion
 
+        #region Static Methods
         private static bool IsValidRegex(string pattern)
         {
             if (string.IsNullOrEmpty(pattern)) return false;
@@ -111,5 +191,13 @@ namespace BetterDefaultBrowser.Lib
 
             return true;
         }
+        #endregion
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
---
+++ BetterDefaultBrowserLib/ManagedFilter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterDefaultBrowser.Lib
+{
+    /// <summary>
+    /// Filter which is managed with the GUI
+    /// </summary>
+    public class ManagedFilter : Filter
+    {
+        private Protocols protocol;
+        private String url;
+        private Ignore flags;
+
+        /// <summary>
+        /// Create a managed filter for an assigned browser.
+        /// </summary>
+        /// <param name="browser">Assigned browser</param>
+        public ManagedFilter(Browser browser) : base(browser) { }
+
+        /// <summary>
+        /// The matching protocols.
+        /// </summary>
+        public Protocols Protocol
+        {
+            get
+            {
+                return protocol;
+            }
+            set
+            {
+                if (!protocol.Equals(value))
+                {
+                    protocol = value;
+                    OnPropertyChanged("Protocol");
+                }
+            }
+        }
+
+        /// <summary>
+        /// The matching url. This must be reduced by the set flags before it can be used in a regular expression.
+        /// </summary>
+        public String URL
+        {
+            get
+            {
+                return url;
+            }
+            set
+            {
+                if (!url.Equals(value))
+                {
+                    url = value;
+                    OnPropertyChanged("URL");
+                }
+            }
+        }
+
+        /// <summary>
+        /// The set flags.
+        /// </summary>
+        public Ignore Flags
+        {
+            get
+            {
+                return flags;
+            }
+            set
+            {
+                if (!flags.Equals(value))
+                {
+                    flags = value;
+                    OnPropertyChanged("Flags");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ignore flags used by managed filters.
+        /// </summary>
+        [Flags]
+        public enum Ignore
+        {
+            SD = 1 << 0,
+            TLD = 1 << 1,
+            Port = 1 << 2,
+            Page = 1 << 3,
+            Parameter = 1 << 4
+        }
+    }
+
+    /// <summary>
+    /// Supported Protocols.
+    /// Used by managed filters.
+    /// </summary>
+    [Flags]
+    public enum Protocols
+    {
+        HTTP = 1 << 0,
+        HTTPS = 1 << 1
+    }
+}
