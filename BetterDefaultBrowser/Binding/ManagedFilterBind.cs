850fc03dd1461bbe999cdabf94fa52dae367782c
Alexander Frank <alexander-pascal.frank@tum.de>
2016-10-09 14:28:34 +0200

    Added new classes to bind GUI-Elements

---
+++ BetterDefaultBrowser/Binding/ManagedFilterBind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterDefaultBrowser.Binding
+{
+    class ManagedFilterBind : INotifyPropertyChanged
+    {
+        #region Handler
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+        #endregion
+    }
+}
