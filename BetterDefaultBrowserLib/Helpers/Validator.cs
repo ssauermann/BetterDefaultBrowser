8e502bbd7e2bb396ed1b722390cd7c14c7c7cd8c
S. Sauermann <saschasauermann@gmx.de>
2016-11-20 03:11:26 +0100

    Committed missing validator class.

---
+++ BetterDefaultBrowserLib/Helpers/Validator.cs
@@ -0,0 +1,12 @@
+﻿namespace BetterDefaultBrowser.Lib.Helpers
+{
+    internal static class Validator
+    {
+        public static bool IsStringMissing(string value)
+        {
+            return
+                string.IsNullOrEmpty(value) ||
+                value.Trim() == string.Empty;
+        }
+    }
+}
