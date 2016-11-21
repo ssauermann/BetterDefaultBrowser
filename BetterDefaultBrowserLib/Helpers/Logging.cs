caafab73cce9831d43f589167f130940d0b2e02d
S. Sauermann <saschasauermann@gmx.de>
2016-11-21 01:28:41 +0100

    Added Serilog as logging library.

---
+++ BetterDefaultBrowserLib/Helpers/Logging.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Configuration;
+
+namespace BetterDefaultBrowser.Lib.Helpers
+{
+    public static class Logging
+    {
+        public static void SetUp()
+        {
+            var conf = new LoggerConfiguration()
+                .WriteTo.File(HardcodedValues.DATA_FOLDER + "log.txt", shared: true)
+                .MinimumLevel.Information();
+
+            // Sets debug level if in debug configuration, else will use information as set above
+            SetDebugLevel(ref conf);
+
+
+            Log.Logger = conf.CreateLogger();
+        }
+
+        [Conditional("DEBUG")]
+        private static void SetDebugLevel(ref LoggerConfiguration conf)
+        {
+            conf.MinimumLevel.Debug();
+        }
+    }
+}
